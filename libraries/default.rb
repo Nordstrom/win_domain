7335f19141a767fa29a15848a4f1aef790f7ab4b
Dave Viebrock <dave.viebrock@nordstrom.com>
2015-09-22 13:40:07 -0700

    initial build

--- libraries/default.rb
+++ libraries/default.rb
@@ -3,6 +3,7 @@
 module Windows
   # helpers for the win_dyndns LWRP
   module Domainjoin
+    # rubocop:disable Metrics/LineLength
     def status?(domain)
       domain = new_resource.domain
       membership = new_resource.membership
@@ -12,7 +13,7 @@ module Windows
         Chef::Log.info "Chef detected domain membership: #{domainmembership} and member server status: #{memberserver_status}"
       elsif membership.eql?('disjoin')
         standaloneserver?
-        Chef::Log.info "Chef detected standalone server status: #{standaloneserver}"
+        Chef::Log.info "Chef detected standalone server status: \"#{standaloneserver}\""
       end
     end
 
@@ -20,15 +21,6 @@ module Windows
       domain = new_resource.domain
       Chef::Log.info "Joining computer to domain: #{domain}"
       powershell_out('*')
-    end
-
-    def domain_disjoined?(*)
-      domain = new_resource.domain
-      Chef::Log.info "Checking for membership in domain: #{domain}"
-      @exists ||= begin
-        cmd = shell_out('*')
-        cmd.stderr.empty? && cmd.stdout.include?(domain)
-      end
     end
 
     def disjoin_domain
@@ -39,16 +31,21 @@ module Windows
 
     def standaloneserver
       # DomainRole == 2 corresponds to "Standalone Server" role
-      myrole = powershell_out('(Get-WmiObject -Class Win32_ComputerSystem -ComputerName $env:ComputerName).DomainRole').stdout.chomp
-      Chef::Log.debug("WSE memberserver function detected this is a member server: #{myrole}")
-      %w(2).include?(myrole)
+      role = powershell_out('(Get-WmiObject -Class Win32_ComputerSystem -ComputerName $env:ComputerName).DomainRole').stdout.chomp
+      nouserdnsdomain = powershell_out('$env:userdnsdomain -eq $nil')
+      Chef::Log.info("Chef detected this server domain role is: #{role}")
+      Chef::Log.info "Chef detected this servers dns domain is: \"#{userdnsdomain}\""
+      %w(2).include?(role) && nouserdnsdomain.eql?('True')
     end
 
     def memberserver_status
       # DomainRole == 3 corresponds to "Member Server" role
-      myrole = powershell_out('(Get-WmiObject -Class Win32_ComputerSystem -ComputerName $env:ComputerName).DomainRole').stdout.chomp
-      Chef::Log.debug("WSE memberserver function detected this is a member server: #{myrole}")
-      %w(3).include?(myrole)
+      domain = new_resource.domain
+      role = powershell_out('(Get-WmiObject -Class Win32_ComputerSystem -ComputerName $env:ComputerName).DomainRole').stdout.chomp
+      userdnsdomain = powershell_out("$env:userdnsdomain -match #{domain}")
+      Chef::Log.info "Chef detected this server domain role is: \"#{role}\""
+      Chef::Log.info "Chef detected this server useruserdnsdomain is: \"#{useruserdnsdomain}\""
+      %w(3).include?(myrole) &&
     end
 
     def domainmembership
@@ -58,5 +55,6 @@ module Windows
       userdomain = powershell_out('$env:userdomain')
       userdnsdomain.stderr.empty? && ((userdnsdomain.stdout.include?(domain)) || (userdomain.stdout.include?(domain)))
     end
+    # rubocop:enable Metrics/LineLength
   end
 end
