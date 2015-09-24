7834ed6cbce605082ac68b2fb60c273612249b2e
Dave Viebrock <dave.viebrock@nordstrom.com>
2015-09-23 18:49:20 -0700

    init build

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ maintainer       'Nordstrom, Inc.'
 maintainer_email 'your_group_dl@nordstrom.com'
 license          'all_rights'
 description      'Installs/Configures win_domain'
-version          '0.1.31'
+version          '0.1.32'
 
 depends 'windows', '~> 1.38'
 depends 'wse_powershell_helper', '~> 0.1'
