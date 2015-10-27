3779333cf832d0c79d1e86e0d254b8bd01b13098
Dave Viebrock <dave.viebrock@nordstrom.com>
2015-10-27 12:27:42 -0700

    adding apache license

--- test/fixtures/cookbooks/testsetup/recipes/default.rb
+++ test/fixtures/cookbooks/testsetup/recipes/default.rb
@@ -13,22 +13,24 @@
 # limitations under the License.
 #
 
-chef_gem 'chef-vault' do
-  version '2.6.1'
-  options("--clear-sources --source #{node['wse_base']['gemserver']}")
-  compile_time true
-end
-require 'chef-vault'
+# uncomment and update the following when using in your environment
 
-user_info = ChefVault::Item.load('WsePasswords', 'WseServerBuilder')
-username = user_info['username']
-password = user_info['password']
-
-win_domain 'nordstrom.net' do
-  ou 'OU=Test,OU=Servers,DC=nordstrom,DC=net'
-  domain 'nordstrom.net'
-  membership 'join'
-  username username
-  password password
-  reboot_delay 10
-end
+# chef_gem 'chef-vault' do
+#   version '2.6.1'
+#   options("--clear-sources --source #{node['wse_base']['gemserver']}")
+#   compile_time true
+# end
+# require 'chef-vault'
+#
+# user_info = ChefVault::Item.load('WsePasswords', 'WseServerBuilder')
+# username = user_info['username']
+# password = user_info['password']
+#
+# win_domain 'nordstrom.net' do
+#   ou 'OU=Test,OU=Servers,DC=nordstrom,DC=net'
+#   domain 'nordstrom.net'
+#   membership 'join'
+#   username username
+#   password password
+#   reboot_delay 10
+# end
