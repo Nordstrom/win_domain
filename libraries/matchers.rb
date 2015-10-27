0c5e1999d64802a83901f7b34860052ccc62d98f
Dave Viebrock <dave.viebrock@nordstrom.com>
2015-10-27 12:44:55 -0700

    fixing branch

--- libraries/matchers.rb
+++
@@ -1,7 +0,0 @@
-if defined?(ChefSpec)
-  ChefSpec.define_matcher(:win_domain)
-
-  def config_win_domain(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:win_domain, :config, resource_name)
-  end
-end
